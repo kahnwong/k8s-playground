f63c29478131c4d05b7b09617dcb06ab39314455
Karn Wong <karn.wong+github@mailbox.org>
2022-09-02 22:08:40 +0700

    add load balancer

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,3 +43,21 @@ resource "kubernetes_deployment" "nginx" {
     }
   }
 }
+
+resource "kubernetes_service" "nginx" {
+  metadata {
+    name = "nginx-example"
+  }
+  spec {
+    selector = {
+      App = kubernetes_deployment.nginx.spec.0.template.0.metadata[0].labels.App
+    }
+    port {
+      node_port   = 30201
+      port        = 80
+      target_port = 80
+    }
+
+    type = "NodePort"
+  }
+}
